147f6dd451c58e47c4d0e3b0401c1d38f04dbfff
Elle Mel <em12129@my.bristol.ac.uk>
2023-01-19 23:19:30 +0000

    Added basic auth and an error for the get request. TODO hide password and add in a time update function for begin and end.

--- html/flights.js
+++ html/flights.js
@@ -11,28 +11,39 @@ $(document).ready(function() {
     getLatestLocation('a835af', function (departure_airport) {
         var location = departure_airport
         console.log('Finally got the location:' +location);
-            $("#oligarchs").append("<table id=\"my_table1\" class=\"flight_table\"></table>");
-            $("#my_table1").append("<tr><th>Oligarch</th><th>Tail Number</th><th>Location</th></tr>");
-            $("#my_table1").append("<tr><td>Elon Musk</td><td>a835af</td><td>" + location + "</td></tr>");
-
+        $("#oligarchs").append("<table id=\"my_table1\" class=\"flight_table\"></table>");
+        $("#my_table1").append("<tr><th>Oligarch</th><th>Tail Number</th><th>Location</th></tr>");
+        $("#my_table1").append("<tr><td>Elon Musk</td><td>a835af</td><td>" + location + "</td></tr>");
         }
     );
     }
 );
 
 function getPlaneInfo(tail_number, callback) {
-    var begin=1673312452
-    var end=1673807597
-    $.get('https://opensky-network.org/api/flights/aircraft?begin='+begin+'&end='+end+'&icao24='+tail_number,
-        function(data, status){
-            console.log('Status:' + status);
-            callback(data[0]);
-        }
-    );
-    return undefined;
+    var begin=1673312908 //#TODO get system time
+    var end=1674169708
+    var uName = 'Elmel'
+    var password = 'fujcoz-9hicfa-zygSin' //#TODO hide password
+    var target_url = 'https://opensky-network.org/api/flights/aircraft?begin='+begin+'&end='+end+'&icao24='+tail_number
+    $.ajax({
+    type: 'GET',
+    url: target_url,
+    headers: {
+        "Authorization": "Basic " + btoa(uName+":"+password)
+    },
+    success : function(data) {
+      console.log('Success:' + data);
+      callback(data[0]);
+    },
+   error: function (xhr,ajaxOptions,throwError){
+    console.log('Error:' + throwError)
+    callback({'estDepartureAirport':"Failed"})
+  },
+});
+
 }
 
-//#TODO add basic auth credentials to GET call
+
 function getLatestLocation(tail_number, callback) {
     getPlaneInfo(tail_number,
         function (incomingData) {
