567b18912a76ef4a84ca01fe93c391daeb1888d7
Elle Mel <em12129@my.bristol.ac.uk>
2023-01-28 21:34:38 +0000

    Replaced callbacks with promises. TODO add more oligarchs, hide password and add in a time update function for begin and end.

--- html/flights.js
+++ html/flights.js
@@ -1,58 +1,66 @@
-function TailNumberToLocation(tail_number) {
-if (tail_number == "a835af") {
-  return "Oakland";}
-  else {
-  return "lost at sea";
-  }
-  }
-
 $(document).ready(function() {
-    console.log("started");
-    getLatestLocation('a835af', function (departure_airport) {
-        var location = departure_airport
-        console.log('Finally got the location:' +location);
-        $("#oligarchs").append("<table id=\"my_table1\" class=\"flight_table\"></table>");
-        $("#my_table1").append("<tr><th>Oligarch</th><th>Tail Number</th><th>Location</th></tr>");
-        $("#my_table1").append("<tr><td>Elon Musk</td><td>a835af</td><td>" + location + "</td></tr>");
-        }
+    var oligarchs = [{'name': 'Alan Sugar', 'tail': 'a62433'},
+    {'name':'Elon Musk', 'tail': 'a835af'},
+    {'name':'Mohammad bin Fahd bin Abdul Aziz Al Saud', 'tail': '4241bb'}]
+//    TODO add bill gates - a17907
+//    var oligarchUpdated = await GetTableData(oligarchs);
+    GetTableData(oligarchs).then(
+            function(oligarchsUpdated) {
+             $("#oligarchs").append("<table id=\"my_table1\" class=\"flight_table\"></table>");
+             $("#my_table1").append("<tr><th>Oligarch</th><th>Tail Number</th><th>Location</th></tr>");
+            for (index in oligarchsUpdated) {
+                var oligarch = oligarchsUpdated[index]
+                    $("#my_table1").append("<tr><td>"
+                        + oligarch.name + "</td><td>"
+                        + oligarch.tail + "</td><td>"
+                        + oligarch.location + "</td></tr>");
+                    console.log(oligarchsUpdated[index])
+                    }
+                console.log('Success')
+            },
+            function(error) { console.log('Promise error on get table data') }
     );
+
+})
+
+async function GetTableData(oligarchsArg) {
+    var oligarchResults = []
+    for (index in oligarchsArg) {
+        var obj = oligarchsArg[index]
+        console.log('hello')
+        var infoPromise = getPlaneInfoPromise(obj['tail']);
+        var resultInfo = await infoPromise;
+        console.log('Await result' + resultInfo);
+        var DeptAirport = resultInfo['estDepartureAirport'];
+        obj.location = DeptAirport
+        oligarchResults.push(obj)
     }
-);
-
-function getPlaneInfo(tail_number, callback) {
-    var begin=1673312908 //#TODO get system time
-    var end=1674169708
-    var uName = 'Elmel'
-    var password = 'fujcoz-9hicfa-zygSin' //#TODO hide password
-    var target_url = 'https://opensky-network.org/api/flights/aircraft?begin='+begin+'&end='+end+'&icao24='+tail_number
-    $.ajax({
-    type: 'GET',
-    url: target_url,
-    headers: {
-        "Authorization": "Basic " + btoa(uName+":"+password)
-    },
-    success : function(data) {
-      console.log('Success:' + data);
-      callback(data[0]);
-    },
-   error: function (xhr,ajaxOptions,throwError){
-    console.log('Error:' + throwError)
-    callback({'estDepartureAirport':"Failed"})
-  },
-});
-
+    return oligarchResults
 }
 
+function getPlaneInfoPromise(tail_number) {
+    let myPromise = new Promise(function(myResolve, myReject) {
+        var begin=1673381781 //#TODO get system time
+        var end=1674932066
+        var uName = 'Elmel'
+        var password = 'fujcoz-9hicfa-zygSin' //#TODO hide password
+        var target_url = 'https://opensky-network.org/api/flights/aircraft?begin='+begin+'&end='+end+'&icao24='+tail_number
+        $.ajax({
+            type: 'GET',
+            url: target_url,
+            headers: {
+                "Authorization": "Basic " + btoa(uName+":"+password)
+            },
+            success : function(data) {
+                console.log('Success:' + data);
+                myResolve(data[0]);
+            },
+            error: function (xhr,ajaxOptions,throwError){
+                console.log('Error:' + throwError)
+                myReject({'estDepartureAirport':"Failed"})
+            },
+        });
 
-function getLatestLocation(tail_number, callback) {
-    getPlaneInfo(tail_number,
-        function (incomingData) {
-            console.log(incomingData);
-            var DeptAirport = incomingData['estDepartureAirport'];
-            console.log('Location:'+ DeptAirport);
-            callback(DeptAirport);
-        }
-    );
-    return undefined;
+    });
+    return myPromise;
 }
-
